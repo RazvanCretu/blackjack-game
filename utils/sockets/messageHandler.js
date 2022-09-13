2b306780b4ec1112bade221f02426234cc07f490
Razvy Cretu <razvan.cretu97@gmail.com>
2022-09-13 23:15:36 +0300

    first commit

---
+++ utils/sockets/messageHandler.js
@@ -0,0 +1,34 @@
+let players = [];
+let rooms = [];
+// let players = {};
+
+const addPlayer = (player) => {
+  players.push(player);
+};
+
+const removePlayer = (playerId) => {
+  if (users.hasOwnProperty(nickname)) {
+    delete users[nickname];
+  }
+};
+
+export default (io, socket) => {
+  const createPlayer = (player) => {
+    addPlayer(player);
+    // rooms.push({
+    //     roomId: player.roomId,
+    //     playersInRoom: []
+    // })
+    // socket.join(player.roomId);
+    socket.emit("created-player", player);
+  };
+
+  const joinRoom = (roomId) => {
+    socket.join(roomId);
+    console.log(io.of("/").adapter.rooms);
+    socket.emit("changed-room", roomId);
+  };
+
+  socket.on("join-room", joinRoom);
+  socket.on("create-player", createPlayer);
+};
